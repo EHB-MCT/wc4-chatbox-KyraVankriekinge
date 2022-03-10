0c3e8008d637ba575f8c04771c8ed02dc9c836e6
Kyra Vankriekinge <kyra.vankriekinge@student.ehb.be>
2022-03-10 11:38:14 +0100

    messages appear

--- js/index.js
+++ js/index.js
@@ -3,15 +3,29 @@
 // url =>  https://ehbchatapp.herokuapp.com/
 
 const chat = {
-    author: "yourName",
+    author: "",
     init() {
+        chat.fetchMessages();
+        chat.renderMessage();
     },
     sendMessage() {
     },
     fetchMessages() {
+        fetch('https://ehbchatapp.herokuapp.com/messages')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                for(let i = data.length - 1; i >= 0; i--){
+                    const messageObject = data[i];
+                    messagesString += '<div class="messageItem"><div class="header"><span class="author">' + messageObject.author  + '</span><span class="time"></span></div><p>' + messageObject.message + '</p></div>';
+                }
+                document.getElementById('messageContainer').innerHTML = messagesString;
+            })
     },
     renderMessage(message) {
+        
     }
-
 }
+
+let messagesString = '';
 chat.init()
